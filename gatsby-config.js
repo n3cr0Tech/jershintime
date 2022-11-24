206138607c30db7978409ab9c8150461f662f8d2
n3cr0Tech <ztxter@gmail.com>
2022-11-23 18:49:52 -0800

    removed pathPrefix - because my github pages is using a custom domain

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  pathPrefix: "/jershintime",
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
