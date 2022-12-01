a90898dc388559ee461f0c2b0b9c2fc03a5d4dbb
n3cr0Tech <ztxter@gmail.com>
2022-11-30 22:39:22 -0800

    Altered grid to only use the Pages section (removed projects section for now)

--- src/@lekoarts/gatsby-theme-jodie/utils/modify-grid.js
+++ src/@lekoarts/gatsby-theme-jodie/utils/modify-grid.js
@@ -1,3 +1,6 @@
-const modifyGrid = (data) => data
+import{
+    onlyPages,
+} from "@lekoarts/gatsby-theme-jodie/src/utils/resolver-templates";
+const modifyGrid = (data) => onlyPages(data);
 
 export default modifyGrid
