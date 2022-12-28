cd03f8c10e6d314fe4c65229a2963b97953304ae
n3cr0Tech <ztxter@gmail.com>
2022-12-27 22:25:07 -0800

    Updates

---
+++ component---src-pages-404-jsx-293e9e46c6df556cd467.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjodie=self.webpackChunkjodie||[]).push([[256],{7291:function(e,t,l){l.r(t);var n=l(7294),u=l(8290);t.default=()=>n.createElement(u.Z,null,n.createElement("div",{style:{textAlign:"center"}},n.createElement("h1",null,"404"),n.createElement("p",null,"Page not found.")))}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-293e9e46c6df556cd467.js.map
